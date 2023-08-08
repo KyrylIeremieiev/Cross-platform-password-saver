915050c5d4903843eff07f492a39f9c186471475
KyrylIeremieiev <34677@ma-web.nl>
2023-08-08 23:55:30 +0200

    add db reading

--- tutorial.hello-world/popup.js
+++ tutorial.hello-world/popup.js
@@ -1,1 +1,8 @@
-console.log("Hello World!")
+table = document.getElementById("table")
+console.log(table)
+fetch("http://localhost:8080/show/light").then(
+            (responce) => {
+            return responce.json();
+        }).then((data) =>{
+            table.innerText = data[0][0]
+        }); 
