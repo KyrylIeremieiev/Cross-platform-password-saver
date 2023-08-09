a5cc356184365f36720796e8adafe5b2641b65c9
KyrylIeremieiev <34677@ma-web.nl>
2023-08-09 12:30:33 +0200

    add button, add post api request with data from inputs using fetch

--- phone/app.js
+++ phone/app.js
@@ -18,7 +18,36 @@ const HelloWorldApp = () => {
             width: '80px',
             height: '20px',
         }}
+        placeholder='Username'
       </TextInput>
+      <TextInput>
+        id={
+            'input2'
+        }
+        style={{
+            width: '80px',
+            height: '20px',
+        }}
+        placeholder='Password'
+      </TextInput>
+      <Button
+        onPress={() => {
+            Username = document.getElementById("input1");
+            Pass = document.getElementById("input2");
+            fetch("localhost:8080/submit/username",
+                {
+                    headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                    },
+                    method: "POST",
+                    body: JSON.stringify({username: Username, pass: Pass})
+                })
+                .then(function(res){ console.log(res) })
+                .catch(function(res){ console.log(res) })
+        }}
+        title="submit"
+        />
     </View>
   );
 };
