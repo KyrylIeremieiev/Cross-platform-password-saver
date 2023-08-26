6debb675f5cc8816ca7d02debe215658fe7ccbed
KyrylIeremieiev <34677@outlook.com>
2023-08-26 22:03:43 +0200

    add form, need testing

--- phone/react-phone/App.js
+++ phone/react-phone/App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {useEffect} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+export default function App(){
+  const [user, setUser] = useState('');
+  const [pass, setPass] = useState('');
+  const submit = () =>{
+      fetch('http://localhost:8080/insert', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({'name': user, 'expansion':pass})
+      });
+  }
+  return(
+    <SafeAreaView>
+    <TextInput
+      onChangeText={(e)=>setUser(e.target.value)}
+      value={setPass}
+    />
+    <TextInput
+      onChangeText={(e)=>setPass(e.target.value)}
+      value={setUser}
+    />
+    <Button
+      onPress={submit}
+      title="submit"
+      color="#841584"
+    />
+  </SafeAreaView>
   );
 }
 
